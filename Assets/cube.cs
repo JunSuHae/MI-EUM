064fbaafb8591a07b0a9bfad82bcf27c790dffd9
702fbtngus <702fbtngus@kaist.ac.kr>
2022-01-15 14:21:38 +0900

    UICamera try

---
+++ Assets/cube.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    void log()
+    {
+        Debug.Log("up");
+    }
+
+}
